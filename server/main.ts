324c87ad9ef4eae80edb834f5692bc7b95bc3063
Andrey <geass1000@gmail.com>
2019-06-27 16:01:57 +0300

    feature(server): add connecting to `NATS` message system

--- server/main.ts
+++ server/main.ts
@@ -3,6 +3,7 @@ import './polyfills';
 import { NestFactory, } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import { environment } from './environments/environment';
+import { Transport } from '@nestjs/microservices';
 
 declare const module: any;
 
@@ -11,6 +12,14 @@ async function bootstrap() {
 
   app.enableCors();
 
+  const microservice = app.connectMicroservice({
+    transport: Transport.NATS,
+    options: <any>{
+      url: 'nats://localhost:4222',
+    },
+  });
+
+  await app.startAllMicroservicesAsync();
   await app.listen(environment.port, () => {
   });
 
