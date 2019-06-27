eb78bbcc83461b49e176bd9fa995cdb17962dc19
Andrey <geass1000@gmail.com>
2019-06-27 13:55:51 +0300

    feature(server): add API Module

--- server/modules/app.module.ts
+++ server/modules/app.module.ts
@@ -1,8 +1,10 @@
+import { ApiModule } from './api/api.module';
 import { AngularUniversalModule } from './angular-universal/angular-universal.module';
 import { Module } from '@nestjs/common';
 
 @Module({
   imports: [
+    ApiModule,
     AngularUniversalModule.forRoot(),
   ],
   controllers: [
