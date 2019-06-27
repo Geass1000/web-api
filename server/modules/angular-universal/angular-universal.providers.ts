f8e58ab117389df5909e5c7e819d6ebfe9b0a1f7
Andrey <geass1000@gmail.com>
2019-06-27 15:16:45 +0300

    feature(server): move `util` logic to factory of `Initializer` provider

--- server/modules/angular-universal/angular-universal.providers.ts
+++ server/modules/angular-universal/angular-universal.providers.ts
@@ -1,16 +1,33 @@
 import { NestApplication, HttpAdapterHost } from '@nestjs/core';
 
-import { AngularUniversalUtils } from './angular-universal.utils';
 import * as Constants from './angular-universal.constants';
 import * as Interfaces from './angular-universal.interfaces';
+
+import { ngExpressEngine } from '@nguniversal/express-engine';
+import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
+
+import * as express from 'express';
 
 export const angularUniversalProviders = [
   {
     provide: Constants.DI.AngularUniversal.Initializer,
     useFactory: (
         httpAdapterHost: HttpAdapterHost,
-        options: Interfaces.AngularUniversal.Options) => {
-      return AngularUniversalUtils.setupUniversal(httpAdapterHost, options);
+        ngOptions: Interfaces.AngularUniversal.Options) => {
+      const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = ngOptions.bundle;
+      const httpAdapter = httpAdapterHost.httpAdapter;
+      const instance: express.Application = httpAdapter.getInstance();
+
+      instance.set('view engine', 'html');
+      instance.set('views', ngOptions.viewsPath);
+      instance.engine('html', <any>ngExpressEngine({
+        bootstrap: AppServerModuleNgFactory,
+        providers: [
+          provideModuleMap(LAZY_MODULE_MAP),
+        ],
+      }));
+
+      instance.get('*.*', express.static(ngOptions.viewsPath));
     },
     inject: [
       HttpAdapterHost,
