11cfa88dd2c42a616cdab2711a35c69be1eab968
Andrey <geass1000@gmail.com>
2019-06-28 14:15:34 +0300

    feature(service): add `Cache` module

--- server/core/core.module.ts
+++ server/core/core.module.ts
@@ -1,10 +1,19 @@
 import * as Nest from '@nestjs/common';
 
+import { CacheModule } from './cache/cache.module';
+
+const nestCacheModule = Nest.CacheModule.register({
+  ttl: 0, max: 0,
+});
+
 @Nest.Global()
 @Nest.Module({
   imports: [
+    nestCacheModule,
+    CacheModule,
   ],
   exports: [
+    nestCacheModule
   ],
 })
 export class CoreModule {}
